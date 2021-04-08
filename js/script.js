175d6e34ccf750d19caf7da1447c83b6438c6148
Daniele Alessi <daniele.alessi@hotmail.it>
2021-04-08 16:59:57 +0200

    definita funzionalità chevron right

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-var button = $('button');
+// salvo in una variabile la chevron secondo regole jQuery 
+var chevronLeft = $('.chevron-left');
+var chevronRight = $('.chevron-right');
 
-button.click(function() {
-    alert("test");
-})
+// CHEVRON RIGHT 
+// al click devo spostare la classe active alla prossima immagine 
+chevronRight.click(function () {
+
+    // devo prima togliere la classe active all'immagine corrente 
+    var currentImage = $('.active');
+    currentImage.removeClass('active');
+
+    // assegno a una variabile l'immagine successiva (elemento html fratello) 
+    var nextImage = currentImage.next();
+
+    // aggiungo classe active 
+    nextImage.addClass('active');
+
+    // all'ultima immagine devo tornare alla prima
+    // se l'immagine corrente ha classe last, rimetto classe active alla prima
+    if (currentImage.hasClass('last')) {
+        var firstImage = $('.first');
+        firstImage.addClass('active');
+    }
+});
+
+// CHEVRON LEFT 
